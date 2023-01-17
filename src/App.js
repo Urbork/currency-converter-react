b02f4432ad0419b00bc2581edc1aea9e131d2fca
Jonasz Ja <jajonaszjj@gmail.com>
2023-01-17 10:14:24 +0100

    Container added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import Container from "./Container";
 import Form from "./Form";
 
 function App() {
   return (
-    <>
+    <Container>
       <h1 className="heading">Kalkulator walutowy</h1>
       <Form />
-    </>
+    </Container>
   );
 }
 
---
+++ src/Container/index.js
@@ -0,0 +1,5 @@
+const Container = ({ children }) => (
+  <main className="container">{children}</main>
+);
+
+export default Container;
