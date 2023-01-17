1f349b9ba7bf2de0615a74102780a34c4a4a2bd2
Jonasz Ja <jajonaszjj@gmail.com>
2023-01-17 14:01:16 +0100

    working on interactions

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,46 +1,69 @@
 import "./style.css";
+import { useState } from "react";
+import { currencies } from "../currencies/currencies";
 
-const Form = () => (
-  <form className="form">
-    <fieldset className="form__fieldset">
-      <legend className="form__legend">Chcę wymienić</legend>
+const Form = () => {
+  const [currency, setCurrency] = useState(currencies[0].shortcut);
+  const [amount, setAmount] = useState("");
+  const [result, setResult] = useState("N/A");
+
+  const onSelectChange = ({ target }) => setCurrency(target.value);
+
+  const onFormChange = () => {
+    const rate = currencies.find(({ shortcut }) => shortcut === currency).rate;
+
+    setResult(amount / rate);
+  };
+
+  return (
+    <form className="form" onChange={onFormChange}>
+      <fieldset className="form__fieldset">
+        <legend className="form__legend">Chcę wymienić</legend>
+        <p>
+          <label className="form__label">
+            Kwota w PLN*:
+            <input
+              className="form__input"
+              type="number"
+              placeholder="podaj kwotę"
+              min="0"
+              step="0.01"
+              autofocus
+              required
+              value={amount}
+              onChange={(event) => setAmount(event.target.value)}
+            />
+          </label>
+        </p>
+        <p>
+          <label className="form__label">
+            Chcę otrzymać:
+            <select
+              className="form__input"
+              name="currency"
+              value={currency}
+              onChange={onSelectChange}
+            >
+              <option value="EUR">EUR - euro</option>
+              <option value="USD">USD - dolar amerykański</option>
+              <option value="GBP">GBP - funt brytyjski</option>
+              <option value="CHF">CHF - frank szwajcarski</option>
+            </select>
+          </label>
+        </p>
+      </fieldset>
+      <p>* - Pole obowiązkowe</p>
       <p>
-        <label className="form__label">
-          Kwota w PLN*:
-          <input
-            className="form__input"
-            type="number"
-            placeholder="podaj kwotę"
-            min="0"
-            step="0.01"
-            autofocus
-            required
-          />
-        </label>
+        <button className="form__button">Przelicz</button>
+        <button className="form__button form__button--reset" type="reset">
+          Wyczyść
+        </button>
       </p>
       <p>
-        <label className="form__label">
-          Chcę otrzymać:
-          <select className="form__input" name="currency">
-            <option value="EUR">EUR - euro</option>
-            <option value="USD">USD - dolar amerykański</option>
-            <option value="GBP">GBP - funt brytyjski</option>
-            <option value="CHF">CHF - frank szwajcarski</option>
-          </select>
-        </label>
+        Wynik: <strong>{result}</strong>
       </p>
-    </fieldset>
-    <p>* - Pole obowiązkowe</p>
-    <p>
-      <button className="form__button">Przelicz</button>
-      <button className="form__button form__button--reset" type="reset">
-        Wyczyść
-      </button>
-    </p>
-    <p>
-      Wynik: <strong>N/A</strong>
-    </p>
-  </form>
-);
+    </form>
+  );
+};
 
 export default Form;
