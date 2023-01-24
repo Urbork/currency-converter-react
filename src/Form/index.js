d1ee960e31b1f504f67fc01ecfcdef078eb771d2
Jonasz Ja <jajonaszjj@gmail.com>
2023-01-24 09:19:11 +0100

    changed form to onSubmit

--- src/Form/index.js
+++ src/Form/index.js
@@ -3,28 +3,40 @@ import { useState } from "react";
 import { currencies } from "../currencies/currencies";
 
 const Form = () => {
-  const [currency, setCurrency] = useState(currencies[0].shortcut);
   const [amount, setAmount] = useState("");
-  const [result, setResult] = useState("N/A");
+  const [currency, setCurrency] = useState(currencies[0].shortcut);
+  const [result, setResult] = useState(0);
 
   const currenciesList = currencies.map((currency) => {
     return (
-      <option value={currency.shortcut}>
+      <option key={currency.id} value={currency.shortcut}>
         {currency.shortcut} - {currency.name}
       </option>
     );
   });
 
-  const onSelectChange = ({ target }) => setCurrency(target.value);
-
-  const onFormChange = () => {
+  const showResult = (currency, amount) => {
     const rate = currencies.find(({ shortcut }) => shortcut === currency).rate;
-
     setResult(amount / rate);
   };
 
+  const onAmountChange = ({ target }) => setAmount(target.value);
+
+  const onSelectChange = ({ target }) => setCurrency(target.value);
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    showResult(currency, amount);
+  };
+
+  const reset = () => {
+    setResult(0);
+    setAmount("");
+    setCurrency(currencies[0].shortcut);
+  };
+
   return (
-    <form className="form" onChange={onFormChange}>
+    <form className="form" onSubmit={onFormSubmit}>
       <fieldset className="form__fieldset">
         <legend className="form__legend">Chcę wymienić</legend>
         <p>
@@ -36,10 +48,10 @@ const Form = () => {
               placeholder="podaj kwotę"
               min="0"
               step="0.01"
-              autofocus
+              autoFocus
               required
               value={amount}
-              onChange={(event) => setAmount(event.target.value)}
+              onChange={onAmountChange}
             />
           </label>
         </p>
@@ -60,12 +72,16 @@ const Form = () => {
       <p>* - Pole obowiązkowe</p>
       <p>
         <button className="form__button">Przelicz</button>
-        <button className="form__button form__button--reset" type="reset">
+        <button
+          className="form__button form__button--reset"
+          type="reset"
+          onClick={reset}
+        >
           Wyczyść
         </button>
       </p>
       <p>
-        Wynik: <strong>{result}</strong>
+        Wynik: <strong>{result.toFixed(2)}</strong>
       </p>
     </form>
   );
