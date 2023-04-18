405d3233dcb87056491e5247693f535e7967e35b
Jonasz Jachym <jajonaszjj@gmail.com>
2023-04-18 10:03:35 +0200

    DateAndTime formatting updated

--- src/DateAndTime/index.js
+++ src/DateAndTime/index.js
@@ -1,22 +1,24 @@
 import { DateElement } from "./styled.js";
 import { useCurrentDate } from "./useCurrentDate.js";
 
+const formatDateAndTime = (date) =>
+  date.toLocaleString(undefined, {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+
 const DateAndTime = () => {
   const currentDate = useCurrentDate();
 
   return (
     <>
       <DateElement className="currentDate">
-        Dziś jest{" "}
-        {currentDate.toLocaleString(undefined, {
-          weekday: "long",
-          day: "numeric",
-          month: "long",
-          year: "numeric",
-          hour: "2-digit",
-          minute: "2-digit",
-          second: "2-digit",
-        })}
+        Dziś jest {formatDateAndTime(currentDate)}
       </DateElement>
     </>
   );
